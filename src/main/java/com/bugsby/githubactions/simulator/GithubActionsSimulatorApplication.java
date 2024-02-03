e488f2555c4fb0ce87f4a8b3ab1d9668c0a8ecb6
Oskar Picus <oskarpicus@yahoo.com>
2024-02-03 09:47:39 +0200

    Initial commit

---
+++ src/main/java/com/bugsby/githubactions/simulator/GithubActionsSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.bugsby.githubactions.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bugsby/githubactions/simulator/GithubActionsSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugsby.githubactions.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
